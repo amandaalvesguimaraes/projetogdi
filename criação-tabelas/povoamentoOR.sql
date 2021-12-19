2cc3144954a1189ab394450f8b311c7b8714792c
belamarib <55455937+belamarib@users.noreply.github.com>
2021-12-19 16:38:16 -0300

    nested table povoamento

--- criação-tabelas/povoamentoOR.sql
+++ criação-tabelas/povoamentoOR.sql
@@ -4,9 +4,9 @@ CREATE SEQUENCE seq INCREMENT by 1 START WITH 1;
 -- INSERÇÃO DE OBJETOS EM TABELA DE OBJETOS tb_Cliente
 INSERT INTO tb_Cliente VALUES (tp_Cliente('123', 'Amanda', 'amanda@email.com', to_date('01/03/2001', 'dd/mm/yy'), tp_lista_endereco(tp_Endereco('111', 1, 'Recife', 'Rua Jornalista Guerra de Holanda', 'Casa Forte',''))));
 INSERT INTO tb_Cliente VALUES (tp_Cliente('234', 'Thaís', 'thais@email.com', to_date('16/07/2001', 'dd/mm/yy'), tp_lista_endereco(tp_Endereco('112', 2, 'Caruaru', 'Avenida Leão Dourado', 'Kennedy','Apartamento 601'))));
-INSERT INTO tb_Cliente VALUES (tp_Cliente('345', 'Isabela', 'isabela@email.com', to_date('26/08/1999', 'dd/mm/yy'), tp_lista_endereco(tp_Endereco('113', 3, 'Recife', 'Avenida 17 de Agosto', 'Casa Forte',''))));
+INSERT INTO tb_Cliente VALUES (tp_Cliente('345', 'Isabela', 'isabela@email.com', to_date('26/08/1999', 'dd/mm/yy'), tp_lista_endereco(tp_Endereco('113', 3, 'Recife', 'Avenida 17 de Agosto', 'Casa Forte',''), tp_Endereco('796', 10, 'Recife', 'Rua Desembargador João Paes', 'Boa Viagem', 'Apartamento 1601'))));
 INSERT INTO tb_Cliente VALUES (tp_Cliente('456', 'Lorena', 'lorena@email.com', to_date('28/07/2000', 'dd/mm/yy'), tp_lista_endereco(tp_Endereco('114', 4, 'Caruaru', 'Avenida Agamenon Magalhães', 'Mauricio de Nassau',''))));
-INSERT INTO tb_Cliente VALUES (tp_Cliente('567', 'Daniel', 'daniel@email.com', to_date('13/10/1998', 'dd/mm/yy'), tp_lista_endereco(tp_Endereco('115', 5, 'Recife', 'Rua General Polidoro', 'Várzea',''))));
+INSERT INTO tb_Cliente VALUES (tp_Cliente('567', 'Daniel', 'daniel@email.com', to_date('13/10/1998', 'dd/mm/yy'), tp_lista_endereco(tp_Endereco('115', 5, 'Recife', 'Rua General Polidoro', 'Várzea',''), tp_Endereco('99', 11, 'Petrolina', 'Rua da Aurora', 'Centro', ''))));
 
 -- INSERÇÃO DE OBJETOS EM TABELA DE OBJETOS tb_Endereco
 INSERT INTO tb_Endereco VALUES (tp_Endereco('111', 1, 'Recife', 'Rua Jornalista Guerra de Holanda', 'Casa Forte',''));
